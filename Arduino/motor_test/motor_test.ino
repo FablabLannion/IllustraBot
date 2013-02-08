ef9b6a776879ade8efa9b9400a8a12b163ff2d44
jerom-labidurie <jerome@labidurie.fr>
2013-02-08 23:25:26 +0100

    interface série pour controler les moteurs

--- Arduino/motor_test/motor_test.ino
+++ Arduino/motor_test/motor_test.ino
@@ -19,38 +19,109 @@
 Stepper motorRight (STEPS_PER_REVOL, 7,4); // dfrobot
 Stepper motorLeft  (STEPS_PER_REVOL, 13,12,11,8); //L298
 
-void movePen();
-
 void setup() {
    // set enables high
    // dfrobot shield
-   pinMode(5, OUTPUT);
-   pinMode(6, OUTPUT);
+   pinMode (5, OUTPUT);
+   pinMode (6, OUTPUT);
    digitalWrite (5, HIGH);
    digitalWrite (6, HIGH);
    // L298N shield
-   pinMode(9, OUTPUT);
-   pinMode(10, OUTPUT);
-   digitalWrite (9, HIGH);
+   pinMode ( 9, OUTPUT);
+   pinMode (10, OUTPUT);
+   digitalWrite ( 9, HIGH);
    digitalWrite (10, HIGH);
    // set the motor speed:
-   motorLeft.setSpeed(SPEED);
-   motorRight.setSpeed(SPEED);
+   motorLeft.setSpeed  (SPEED);
+   motorRight.setSpeed (SPEED);
+   // init serial
+   Serial.begin(9600);
 }
 
 void loop() {
-   movePen ();
-   delay(500);
+   char c=0;
+   int p1,p2;
+   
+   while ( (Serial.available() > 0) && (c != '\n') )
+   {
+      c = Serial.read();
+      switch (c)
+      {
+         case 'D': // move right motor (rotations)
+         case 'd': // move right motor (steps)
+            p1 = Serial.parseInt();
+            p1 = (c=='d')? p1 : STEPS_PER_REVOL*p1;
+            moveRight (p1);
+            break;
+         case 'G': // move left motor (rotations)
+         case 'g': // move left motor (steps)
+            p1 = Serial.parseInt();
+            p1 = (c=='g')? p1 : STEPS_PER_REVOL*p1;
+            moveLeft (p1);
+            break;
+         case 'T': // move both motors (rotations)
+         case 't': // move both motors (steps)
+            p1 = Serial.parseInt();
+            p2 = Serial.parseInt();
+            p1 = (c=='t')? p1 : STEPS_PER_REVOL*p1;
+            p2 = (c=='t')? p2 : STEPS_PER_REVOL*p2;
+            movePen (p1, p2);
+            break;
+         case '\n':
+            break;
+         default:
+            Serial.println("?");
+            break;
+      }
+//       Serial.println ("");
+   }
+} /* loop */
+
+/** turn the 2 motors following a line.
+ * The 2 motors rotation time will be the same no matter what the parameters are.
+ * @param rSteps : nb of steps for right motor
+ * @param lSteps : nb of steps for left motor
+ */
+void movePen(int rSteps, int lSteps) {
+   int max=0,  // nombre total d'iteration
+       rPas=0, // nombre de steps par itération
+       lPas=0; // nombre de steps par itération
+   
+   if (abs(rSteps) < abs(lSteps)) {
+      max = abs(rSteps);
+      rPas = (rSteps<0)? -1 : 1;
+      lPas = lSteps / abs(rSteps);
+   }
+   else {
+      max = abs(lSteps);
+      lPas = (lSteps<0)? -1 : 1;
+      rPas = rSteps / abs(lSteps);
+   }
+   // DEBUG: affiche le ratio
+   Serial.print (rPas); Serial.print("/");Serial.println(lPas);
+   for (int c=0;c<max;c++) {
+      motorLeft.step(lPas);
+      motorRight.step(rPas);
+   }
+} //movePen
+
+/** turn the right motor
+ * @param steps : number of steps
+ */
+void moveRight (int steps) {
+   for (int c=0;c<abs(steps);c++) {
+      motorRight.step((steps<0)?-1:1);
+   }   
 }
 
-/** turn the 2 motors
+/** turn the left motor
+ * @param steps : number of steps
  */
-void movePen() {
-   for (int c=0;c<100;c++) {
-      motorLeft.step(-STEPS_PER_REVOL/100);
-      motorRight.step(STEPS_PER_REVOL/100);
-   }
-} //move
+void moveLeft (int steps) {
+   for (int c=0;c<abs(steps);c++) {
+      motorLeft.step((steps<0)?-1:1);
+   }   
+}
 
 
 
--- Arduino/motor_test/readme.txt
+++ Arduino/motor_test/readme.txt
@@ -1,2 +1,14 @@
 Petit sketch de test arduino pour les moteurs pas-à-pas.
 
+Commande par liaison série (9600 8N1)
+
+chaque commande est une lettre suivie d'un ou 2 paramètres numériques
+
+Lettre majuscule : paramètre en nombre de révolutions
+lettre minuscule : paramètre en nombre de steps (200 steps par révolution)
+paramètre positif : sens horaire
+paramètre négatif : sens anti-horaire
+
+D nb            moteur droit
+g nb            moteur gauche
+t nb1 nb2       les 2 moteurs
