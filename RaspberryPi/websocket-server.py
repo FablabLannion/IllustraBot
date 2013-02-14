03bdf48431028318301f2abbc23e70faa24dfaf6
davidblaisonneau <david.blaisonneau@gmail.com>
2013-02-14 23:02:03 +0100

    update websocket server to manage different kinds of robots and different kinds of connectors

--- RaspberryPi/websocket-server.py
+++ RaspberryPi/websocket-server.py
@@ -6,78 +6,177 @@ import tornado.ioloop
 import tornado.web
 import re
 import math
-import serial
+import socket
 
-return_text = 0
-drawBot = 1
-arduino_dev = '/dev/ttyUSB0'
-arduino_dev_speed = 115200
-source_maxX = 500
-source_maxY = 700
-target_maxX = 50
-target_maxY = 30
+return_text = 1
+botname = 'test'
+sourceMaxX = 500
+sourceMaxY = 700
+config = { 'drawbot': {
+				'type': 'dev',
+				'arduinoDev': '/dev/ttyUSB0',
+				'arduinoDevSpeed': 115200,
+				'sizeX': 50,
+				'sizeY': 30,
+				'inverseAxes': 0,
+				'trapezeFactor': 0,
+				'floor': 1,
+				'moveToLenght': 1,
+				'extraLenght': 5,
+				},
+			'reprap': {
+				'type': 'dev',
+				'arduinoDev': '/dev/ttyUSB0',
+				'arduinoDevSpeed': 115200,
+				'sizeX': 20,
+				'sizeY': 20,
+				'inverseAxes': 0,
+				'trapezeFactor': 0,
+				'floor': 0,
+				'moveToLenght': 0,
+				},
+			'lightbot': {
+				'type': 'socket',
+				'parrotSocket': '192.168.1.100:22300',
+				'sizeX': 20,
+				'sizeY': 20,
+				'inverseAxes': 1,
+				'trapezeFactor': 2,
+				'floor': 0,
+				'moveToLenght': 0,
+				},
+			'test': {
+				'type': 'socket',
+				'socketIP': '127.0.0.1',
+				'socketPort': 9999,
+				'sizeX': 20,
+				'sizeY': 20,
+				'inverseAxes': 0,
+				'trapezeFactor': 0,
+				'floor': 0,
+				'moveToLenght': 0,
+				},
+		}
 
 class GCodeHGandler():
 	# Parse the raw GCode and do some stuff before sending to robot
 	def parseRaw(self, rawLine):
 		self.raw = rawLine
-		matchObj = re.match( r'.*g0x(.*)y(.*)z(.*)', rawLine)
+		matchObj = re.match( r'.*g0x(.*)y(.*)z(.*)', rawLine, re.M|re.I)
 		self.x = float(matchObj.group(1))
 		self.y = float(matchObj.group(2))
 		self.z = float(matchObj.group(3))
 		self.scale()
-		if drawBot:
-			self.toDrawBotLength()
+		if config[botname]['inverseAxes']:
+			self.inverseAxes()
+		if config[botname]['floor']:
+			self.floor()
 		self.prepareGCode()
 		self.debugGCode()
 			
 	# Prepare the GCode depending on the target (DrawBot or Reprap)
 	def prepareGCode(self):
-		if drawBot:
-			self.gcode = 'l ' + str(int(math.floor(self.l1))) + ' '+ str(int(math.floor(self.l2)))
+		if config[botname]['moveToLenght']:
+			self.toDrawBotLength()
+			self.gcode = 'l ' + str(self.l1) + ' '+ str(self.l2)
 		else:
 			self.gcode = 'G0X' + str(self.x) + 'Y'+ str(self.y) + 'Z' + str(self.z)
 	
+	# inverse axes
+	def inverseAxes(self):
+		tmp = self.z
+		self.z = self.y
+		self.y = tmp
+
+	# Floor values
+	def floor(self):
+		if config[botname]['moveToLenght']:
+			self.l1 = int(math.floor(self.l1))
+			self.l2 = int(math.floor(self.l2))
+	
 	# Prepare DrawBot length
 	def toDrawBotLength(self):
-		self.l1 = math.sqrt(math.pow(self.x,2)+math.pow(self.y,2))
-		self.l2 = math.sqrt(math.pow(target_maxX-self.x,2)+math.pow(self.y,2))
+		self.l1 = math.sqrt(math.pow(self.x,2)+math.pow(self.y,2)) + config[botname]['extraLenght']
+		self.l2 = math.sqrt(math.pow(config[botname]['sizeX']-self.x,2)+math.pow(self.y,2)) + config[botname]['extraLenght']
 
 	# Scale the gcode to the target
 	def scale(self):
-		self.x = self.x*target_maxX/source_maxX
-		self.y = self.y*target_maxY/source_maxY
+		# Scale
+		self.x = self.x*config[botname]['sizeX']/sourceMaxX
+		self.y = self.y*config[botname]['sizeY']/sourceMaxY
+		# Keep in bounds
+		if self.x > config[botname]['sizeX']:
+			self.x = config[botname]['sizeX']
+		if self.y > config[botname]['sizeY']:
+			self.y = config[botname]['sizeY']
+		#~ # Projection to trapeze
+		if config[botname]['trapezeFactor']:
+			self.x = (self.x-config[botname]['sizeX']/2)\
+						*(1+(config[botname]['trapezeFactor']-1)*self.y/config[botname]['sizeY'])\
+						+config[botname]['trapezeFactor'] * config[botname]['sizeX']/2
 
 	# Print out
 	def debugGCode(self):
 		print(self.gcode)
 
-
+class Robot():
+	# init communication with the robot
+	def init(self):
+		if config[botname]['type'] == 'dev':
+			self.dev = serial.Serial(config[botname]['arduinoDev'], config[botname]['arduinoDevSpeed'])
+		elif config[botname]['type'] == 'file':
+			self.f = open(config[botname]['file'], 'w')
+		elif config[botname]['type'] == 'socket':
+			self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.socket.connect((config[botname]['socketIP'],config[botname]['socketPort']))
+	
+	# send GCode and wait for ack
+	def write(self, gcode):
+		if config[botname]['type'] == 'dev':
+			self.dev.write(gcode)
+			self.readLine = self.dev.readline()
+		elif config[botname]['type'] == 'file':
+			self.f.write(gcode+"\n")
+			self.f.flush()
+			self.readLine = 'this is a file, can not read robot return'
+		elif config[botname]['type'] == 'socket':
+			self.socket.sendall(bytes(gcode+"\n", 'UTF-8'))
+			self.readLine = self.socket.recv(1024).decode('UTF-8')
+	
+	# Close communication with the robot
+	def close(self):
+		if config[botname]['type'] == 'dev':
+			self.dev.close()
+		elif config[botname]['type'] == 'file':
+			self.f.close()
+		elif config[botname]['type'] == 'socket':
+			self.socket.close()
+	
 class WSHandler(tornado.websocket.WebSocketHandler):
 	# When the sebsocket is opened
 	def open(self):
 		print('New connection was opened')
 		self.write_message("Welcome to my websocket!")
-		self.dev = serial.Serial(arduino_dev, arduino_dev_speed)
+		robot.init()
 
 	# When the websocket receive a message
 	#   TODO: redirect gcode to /dev/spidev0.0 ou 0.1
 	def on_message(self, message):
-		gcode_h.parseRaw(message)
-		self.dev.write(gcode_h.gcode)
 		print('Incoming message:', message)
+		gcode_h.parseRaw(message)
+		robot.write(gcode_h.gcode)
 		print("I send: " + gcode_h.gcode)
-		print("I get: " + self.dev.readline())
+		print("I get: " + robot.readLine)
 		# Write GCode to TTYUSB
 		if return_text:
 			self.write_message("You said: " + message)
 			self.write_message("I send: " + gcode_h.gcode)
-			self.write_message("I get: " + self.dev.readline())
+			self.write_message("I get: " + robot.readLine)
 	
 	# When the websocket is closed
 	def on_close(self):
 		print('Connection was closed...')
-		self.dev.close
+		robot.close()
 
 # MAIN
 application = tornado.web.Application([
@@ -85,6 +184,7 @@ application = tornado.web.Application([
 ])
 
 if __name__ == "__main__":
+	robot = Robot()
 	gcode_h = GCodeHGandler()
 	http_server = tornado.httpserver.HTTPServer(application)
 	http_server.listen(22300)
