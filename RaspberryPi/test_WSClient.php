81b92700fcf0577948f8c183bd18ae0b63d57d24
davidblaisonneau <david.blaisonneau@gmail.com>
2013-03-12 00:09:45 +0100

    add ws client

--- RaspberryPi/test_WSClient.php
+++ RaspberryPi/test_WSClient.php
@@ -3,12 +3,34 @@
     <title>WebSockets with Python & Tornado</title>
     <meta charset="utf-8" />
     <style type="text/css">
-      body {
-        text-align: center;
-        min-width: 500px;
-      }
+		body {
+			min-width: 600px;
+			text-align: center;
+		}
+		#gcode {
+			display: inline-block;
+		}
+		#log {
+			width:400px;
+			height:400px;
+			overflow:scroll;
+			right:20px;
+			margin:auto;
+			text-align:left;
+			background-color:#eeeeee; 
+		}
+		#msg{
+			width:100%;
+		}
+		#mycanvas{
+			border: 1px solid gray;
+			width: 400px;
+			height: 400px;
+			margin: 5;
+		}
     </style>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script src="jquery.js"></script>
+    <script src="jcanvas.min.js"></script>
     <script>
       $(function(){
         var ws;
@@ -29,9 +51,8 @@
           $("#msg").val(msg);
         }
 
-        ws = new WebSocket("ws://mypi:22300/ws");
+        ws = new WebSocket("ws://127.0.0.1:22300/ws");
         ws.onmessage = function(evt) {
-
           logger(evt.data);
         };
         ws.onclose = function(evt) { 
@@ -49,19 +70,102 @@
         $("#thebutton").click(function(){
           sender();
         });
+        
+        $("canvas").drawRect({
+			layer: true,
+			fillStyle: "#eee",
+			x: 0, y: 0,
+			width: 400,
+			height: 400,
+			fromCenter: false,
+			click: function(layer) {
+		        var dx, dy, dist;
+				dx = layer.eventX - layer.x;
+				dy = layer.eventY - layer.y;
+				console.info(dx +', '+ dy);
+			}
+		});
+		$("canvas").drawArc({
+			layer: true,
+			fillStyle: "gray",
+			x: 0, y: 0,
+			radius: 10,
+			click: function(layer) { ws.send("G0X0Y0Z0"); }
+		});
+		$("canvas").drawArc({
+			layer: true,
+			fillStyle: "gray",
+			x: 200, y: 0,
+			radius: 10,
+			click: function(layer) { ws.send("G0X200Y0Z0"); }
+		});
+		$("canvas").drawArc({
+			layer: true,
+			fillStyle: "gray",
+			x: 400, y: 0,
+			radius: 10,
+			click: function(layer) { ws.send("G0X400Y0Z0"); }
+		});
+		$("canvas").drawArc({
+			layer: true,
+			fillStyle: "gray",
+			x: 0, y: 200,
+			radius: 10,
+			click: function(layer) { ws.send("G0X0Y200Z0"); }
+		});
+		$("canvas").drawArc({
+			layer: true,
+			fillStyle: "gray",
+			x: 200, y: 200,
+			radius: 10,
+			click: function(layer) { ws.send("G0X200Y200Z0"); }
+		});
+		$("canvas").drawArc({
+			layer: true,
+			fillStyle: "gray",
+			x: 400, y: 200,
+			radius: 10,
+			click: function(layer) { ws.send("G0X400Y200Z0"); }
+		});
+		$("canvas").drawArc({
+			layer: true,
+			fillStyle: "gray",
+			x: 0, y: 400,
+			radius: 10,
+			click: function(layer) { ws.send("G0X0Y400Z0"); }
+		});
+		$("canvas").drawArc({
+			layer: true,
+			fillStyle: "gray",
+			x: 200, y: 400,
+			radius: 10,
+			click: function(layer) { ws.send("G0X200Y400Z0"); }
+		});
+		$("canvas").drawArc({
+			layer: true,
+			fillStyle: "gray",
+			x: 400, y: 400,
+			radius: 10,
+			click: function(layer) { ws.send("G0X400Y400Z0"); }
+		});
       });
     </script>
   </head>
 
   <body>
-    <h1>WebSockets with Python & Tornado</h1>
-    <div id="log" style="overflow:scroll;width:500px; height:200px;background-color:#ffeeaa; margin:auto; text-align:left">Messages go here</div>
-
-    <div style="margin:10px">
-      <input type="text" id="msg" style="background:#fff;width:200px"/>
-      <input type="button" id="thebutton" value="Send" />
-    </div>
-
-    <a href="http://lowpowerlab.com/blog/2013/01/17/raspberrypi-websockets-with-python-tornado/">www.LowPowerLab.com</a>
+    <h1 id='title'>Illustrabot Web Client</h1>
+    <div style='display: bloc;'>
+		<div id="gcode">
+			<div id="log">logs</div>
+			<div id="manual-gcode">
+			  <input type="text" id="msg" value="G0X0Y0Z0" style="background:#fff;width:200px">
+			  <input type="button" id="thebutton" value="Send" />
+			</div>
+		</div>
+		<canvas id='mycanvas' width=400 height=400>
+			This text is displayed if your browser does not support HTML5 Canvas.
+		</canvas>
+	</div>
+    <a href="http://lowpowerlab.com/blog/2013/01/17/raspberrypi-websockets-with-python-tornado/">Inspired from www.LowPowerLab.com</a>
   </body>
 </html>
