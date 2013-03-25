cb955dc305cb6849a2840b2b39ee7d0257439a94
Jérôme <jerome@labidurie.fr>
2013-03-25 22:02:09 +0100

    Add crc to test

--- RaspberryPi/spi_test/tspi.py
+++ RaspberryPi/spi_test/tspi.py
@@ -2,13 +2,13 @@ import sys
 import spi
 
 spi.initialize(0,32,250000,0)
-str=sys.argv[1] + '\n'
+str=sys.argv[1]
 crc = 0
-for b in sys.argv[1][-7:]:
+for b in sys.argv[1]:
 	crc += ord(b)
-crc &= 0xF
+crc &= 0xFF
 print("CRC:",crc)
-list_c = tuple(map(ord,list(str))+[crc])
+list_c = tuple(map(ord,list(str))+[crc,10])
 print list_c
 spi.transfer(list_c)
 spi.end()
